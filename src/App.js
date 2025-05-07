ce9f7ac7f5d458bfb781a2863f42d6e6ad5e0840
yakovglee <yakoglee@gmail.com>
2025-05-07 22:17:00 +0900

    upd

--- src/App.js
+++ src/App.js
@@ -1,39 +1,28 @@
+import { useState } from "react";
+
+import AppSider from "./components/AppSider";
 import FlipCard from "./components/FlipCard";
 
 import { data } from "./data";
 
-import { Layout, Menu, Flex } from "antd";
-const { Content, Sider, Header } = Layout;
-
-const items = [1, 2, 3, 4].map((icon, index) => ({
-    key: String(index + 1),
-    label: `nav ${index + 1}`,
-}));
+import { Layout, Flex } from "antd";
+import AppHeader from "./components/AppHeader";
+const { Content } = Layout;
 
 function App() {
+    const [collapsed, setCollapsed] = useState(true);
     return (
-        <Layout>
-            <Sider
-                breakpoint="lg"
-                collapsedWidth="0"
-                onBreakpoint={(broken) => {
-                    console.log(broken);
-                }}
-                onCollapse={(collapsed, type) => {
-                    console.log(collapsed, type);
-                }}
-            >
-                <div className="demo-logo-vertical" />
-                <Menu
-                    theme="dark"
-                    mode="inline"
-                    defaultSelectedKeys={["4"]}
-                    items={items}
-                />
-            </Sider>
+        <Layout style={{ minHeight: "100vh" }}>
+            <AppSider collapsed={collapsed} />
             <Layout>
+                <AppHeader collapsed={collapsed} setCollapsed={setCollapsed} />
                 <Content style={{ margin: "24px 16px 0" }}>
-                    <Flex justify={'center'} align={'flex-start'} wrap gap="middle">
+                    <Flex
+                        justify={"center"}
+                        align={"flex-start"}
+                        wrap
+                        gap="middle"
+                    >
                         <FlipCard />
                         <FlipCard />
                         <FlipCard />
