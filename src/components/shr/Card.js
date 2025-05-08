3d01d10451661daf590bae157ec6238247e64bef
yakovglee <yakoglee@gmail.com>
2025-05-08 19:43:40 +0900

    upd: add max height to card body

--- src/components/shr/Card.js
+++ src/components/shr/Card.js
@@ -3,7 +3,16 @@ import { Card } from "antd";
 function CardCustom({ children }) {
     return (
         <>
-            <Card className="h-full min-w-fit rounded-[36px] shadow-xl flex items-center justify-center text-center">
+            <Card
+                className="h-full min-w-fit max-h-full max-w-full rounded-[36px] shadow-xl flex items-center justify-center text-center"
+                styles={{
+                    body: {
+                        maxHeight: "100%",
+                        overflow: "auto",
+                        width: "100%",
+                    },
+                }}
+            >
                 {children}
             </Card>
         </>
